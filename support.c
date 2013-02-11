e16a734263c70f6c86a7672ff84bd5dc122b51cb
Sebastian Bauer <mail@sebastianbauer.info>
2013-02-11 12:23:33 +0100

    Made debug macro optional.

--- support.c
+++ support.c
@@ -15,7 +15,11 @@
 
 #include "support.h"
 
+#ifdef DEBUG
 #define D(txt,...) fprintf(stderr,txt,__VA_ARGS__)
+#else
+#define D(txt,...)
+#endif
 
 #define MIN(a,b) ((a)<(b)?(a):(b))
 #define MAX(a,b) ((a)>(b)?(a):(b))
